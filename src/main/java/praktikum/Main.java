2b9bc0dc3feaa71a5ad672f00fef731b0ae2cae0
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2026-09-16 19:32:44 +0200

    greet the world again

--- src/main/java/praktikum/Main.java
+++ src/main/java/praktikum/Main.java
@@ -3,5 +3,6 @@ package praktikum;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Goodbye world");
+        System.out.println("Hello world");
     }
 }
